b5995c382550a462711317c6ac898acf32df9650
AioiLight <info@aioilight.space>
2020-11-17 23:02:47 +0900

    メッセージの返答まで

--- YukiChang/Program.cs
+++ YukiChang/Program.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
+using Newtonsoft.Json;
 
 namespace YukiChang
 {
@@ -14,7 +15,11 @@ namespace YukiChang
 
 		public async Task MainAsync()
 		{
-			var _config = new DiscordSocketConfig { MessageCacheSize = 100 };
+			Settings = File.Exists("settings.json")
+                ? JsonConvert.DeserializeObject<Settings>(File.ReadAllText("settings.json", Encoding.UTF8))
+                : new Settings();
+
+            var _config = new DiscordSocketConfig { MessageCacheSize = 100 };
 			Client = new DiscordSocketClient(_config);
 
 			await Client.LoginAsync(TokenType.Bot, Token);
@@ -25,16 +30,31 @@ namespace YukiChang
 				Console.WriteLine("Bot is connected!");
 				return Task.CompletedTask;
 			};
+
+            Client.MessageReceived += Client_MessageReceived;
 			
 			await Task.Delay(-1);
 		}
 
-		private static string Token
+        private Task Client_MessageReceived(SocketMessage arg)
+        {
+			var text = arg.Content.Trim();
+			if (text.StartsWith("!yuki"))
+            {
+				arg.Channel.SendMessageAsync("pong!");
+            }
+
+			return Task.CompletedTask;
+        }
+
+        private static string Token
 		{
 			get
 			{
 				return File.ReadAllText("token.txt", Encoding.UTF8);
 			}
 		}
+
+		private static Settings Settings { get; set; }
 	}
 }
