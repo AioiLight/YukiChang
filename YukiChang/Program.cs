888d91a9c2f2df5e2e7b0ba0068a81ff0352ba3c
AioiLight <info@aioilight.space>
2020-11-17 22:42:11 +0900

    イベント駆動

--- YukiChang/Program.cs
+++ YukiChang/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 
 namespace YukiChang
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+		private DiscordSocketClient Client;
+		static void Main() => new Program().MainAsync().GetAwaiter().GetResult();
+
+		public async Task MainAsync()
+		{
+			var _config = new DiscordSocketConfig { MessageCacheSize = 100 };
+			Client = new DiscordSocketClient(_config);
+
+			await Client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("YukiChang"));
+			await Client.StartAsync();
+
+			Client.Ready += () =>
+			{
+				Console.WriteLine("Bot is connected!");
+				return Task.CompletedTask;
+			};
+			
+			await Task.Delay(-1);
+		}
+	}
 }
