fa365a32c4beb2b6faf9f8643021986a2d307f5e
AioiLight <info@aioilight.space>
2021-06-27 00:27:52 +0900

    + Player

---
+++ YukiChang/Player.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YukiChang
+{
+    /// <summary>
+    /// プリンセスコネクト！Re:Dive のプレイヤー情報を格納するクラス。
+    /// </summary>
+    public class Player
+    {
+        public Player(ulong uid)
+        {
+            UID = uid;
+        }
+
+        /// <summary>
+        /// ボスを討伐した。
+        /// </summary>
+        internal void Kill()
+        {
+            if (LastAttackCount < 3)
+            {
+                LastAttackCount++;
+            }
+        }
+
+        /// <summary>
+        /// ラストアタックを消費した。
+        /// </summary>
+        internal void ConsumeLastAttack()
+        {
+            if (LastAttackRemain > 0)
+            {
+                DoneLastAttackCount++;
+            }
+        }
+
+        /// <summary>
+        /// UID。
+        /// </summary>
+        public ulong UID { get; private set; }
+        /// <summary>
+        /// 持越しを持った回数。
+        /// </summary>
+        public uint LastAttackCount { get; private set; }
+        /// <summary>
+        /// 持越しを消費した回数。
+        /// </summary>
+        public uint DoneLastAttackCount { get; private set; }
+        /// <summary>
+        /// 残りの持越し数。
+        /// </summary>
+        public uint LastAttackRemain
+        {
+            get
+            {
+                return LastAttackCount - DoneLastAttackCount;
+            }
+        }
+    }
+}
